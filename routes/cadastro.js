2a0386f4eb904be9bb1498756f39a23858a5fab2
luan.nsct <luan.nsct@gmail.com>
2024-06-21 13:56:56 -0300

    criando schema d ecadastro de um cliente

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -1,8 +1,9 @@
 var express = require('express');
-const { criarNovoUsuario } = require('../program/middleware/criar/usuario/criarnovoUsuario');
-const { criarcadastroPro } = require('../program/middleware/criar/Loja/criarNovaLoja')
-const { schemaCriarNovoUsuario } = require('../program/schemas/schemasusuario');
-const { schemaCriarUsuarioPro } = require('../program/schemas/schemasLoja')
+const { criarNovoUsuario } = require('../app/middleware/criar/usuario/criarnovoUsuario');
+const { criarcadastroPro } = require('../app/middleware/criar/Loja/criarNovaLoja');
+const { schemaCriarNovoUsuario } = require('../app/schemas/schemasusuario');
+const { schemaCriarUsuarioPro } = require('../app/schemas/schemasLoja');
+const { schemaDadosCliente } = require('../app/schemas/schemasClientes');
 var cadastro = express.Router();
 
 /* GET home page. */
@@ -11,6 +12,18 @@ cadastro.post('/cadastrarusuario', criarNovoUsuario(schemaCriarNovoUsuario), (re
 })
 cadastro.post('/cadastroPro', criarcadastroPro(schemaCriarUsuarioPro), (req, res) => {
     return res.status(200).json({ message: 'Teste concluido com sucesso!' });
+})
+
+cadastro.post('/enderecoEntrega', async (req, res, next) => {
+    try {
+        const teste = await schemaDadosCliente.validateAsync(req.body);
+        next();
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+}, (req, res) => {
+    return res.status(200).json({ message: "Teste cadastro cliente concluido com sucesso!" })
 })
 
 module.exports = cadastro;
